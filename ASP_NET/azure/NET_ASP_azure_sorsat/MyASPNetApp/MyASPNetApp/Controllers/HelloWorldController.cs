70a5b8897296cfba668bdba8bba6dbc38f0acd1b
psavela <petri.m.savela@gmail.com>
2015-11-27 14:35:03 +0200

    27.11.2015 2nd

---
+++ ASP_NET/azure/NET_ASP_azure_sorsat/MyASPNetApp/MyASPNetApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyASPNetApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+/*        public string Index()
+        {
+            return "This is a <strong>DEFAULT INDEX </strong> Index View";
+        }
+*/
+        /*
+                public string Welcome()
+                {
+                    return "This is a Welcome View";
+                }
+
+            */
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /*
+                public string Welcome(string name, int ID = 1)
+                {
+                    return HttpUtility.HtmlEncode("Hello " + name + ", ID is: " + ID);
+                }
+        */
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello" + name;
+            ViewBag.numTimes = numTimes;
+            return View();
+        }
+
+        public ActionResult MyTestView()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP_NET/azure/NET_ASP_azure_sorsat/MyASPNetApp/MyASPNetApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyASPNetApp.Startup))]
+namespace MyASPNetApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
